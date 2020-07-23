33280a41973a627a179d738dc4f60a3a4f92e334
Santoshkumar Vagga <vaggasantoshkumar@outlook.com>
2020-07-23 16:36:24 +0530

    k

---
+++ DEVELOPER_BASICS/Composition.py
@@ -0,0 +1,15 @@
+
+"""
+Composition - how to create complex objects from simpler ones.
+
+Unlike Inheritance, which holds a "is a" relationship; Composition holds "has a" relationship.
+
+Composition is having an object passed as a parameter to another object so that we can have an
+abstraction of details of the passing class in the child/destination class. It is more secure when
+compared to inheritance model,where we import all of parent class's attributes and
+methods exclusively inside child class.
+
+However, we can combine both inheritance and composition,as per needs.
+
+"""
+
