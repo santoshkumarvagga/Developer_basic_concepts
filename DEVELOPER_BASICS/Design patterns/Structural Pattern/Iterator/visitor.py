83a3ca6909f0b319e2b531ca74884a6ed804e840
Santoshkumar Vagga <vaggasantoshkumar@outlook.com>
2020-08-13 16:21:41 +0530

    k

--- DEVELOPER_BASICS/Design patterns/Structural Pattern/Iterator/visitor.py
+++ DEVELOPER_BASICS/Design patterns/Structural Pattern/Iterator/visitor.py
@@ -1,13 +1,13 @@
 class House(object): #The class being visited 
 	def accept(self, visitor):
 		"""Interface to accept a visitor"""
-		visitor.visit(self) #Triggers the visiting operation!
+		#Triggers the visiting operation!
 
 	def work_on_hvac(self, hvac_specialist):
 		print(self, "worked on by", hvac_specialist) #Note that we now have a reference to the HVAC specialist object in the house object!
 
 	def work_on_electricity(self, electrician):
-		print(self, "worked on by", electrician) #Note that we now have a reference to the electrician object in the house object!
+		#Note that we now have a reference to the electrician object in the house object!
 
 	def __str__(self):
 		"""Simply return the class name when the House object is printed"""
@@ -30,18 +30,20 @@ class HvacSpecialist(Visitor): #Inherits from the parent class, Visitor
 class Electrician(Visitor): #Inherits from the parent class, Visitor
 	"""Concrete visitor: electrician"""
 	def visit(self, house):
-		house.work_on_electricity(self) #Note that the visitor now has a reference to the house object
+		#Note that the visitor now has a reference to the house object
 
 #Create an HVAC specialist
-hv = HvacSpecialist()
+
 #Create an electrician
-e = Electrician()
+
 
 #Create a house
-home = House()
+
 
 #Let the house accept the HVAC specialist and work on the house by invoking the visit() method
-home.accept(hv)
+
 
 #Let the house accept the electrician and work on the house by invoking the visit() method
-home.accept(e)
+
+
+
